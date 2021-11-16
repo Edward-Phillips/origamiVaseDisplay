1065f287db6bbb0a610122b49eb2993b1755447f
edwardPhillips <edward@twine.fm>
2021-11-16 00:47:10 +0000

    add noreferrer to link

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ export default function About() {
       <p> I am a full stack developer, currently working in react and Laravel. Most of my side projects are written in react or node. I have recently been implementing typescript where convenient and am a big fan.
       </p>
       <p>
-        I swear I'm more interesting than this, I'm just really struggling at the moment to think of anything, I'll come back  here when I think of something interesting, in the meantime why not take a minute or two out of your day and watch <a target="_blank" href="https://www.youtube.com/watch?v=1JOwfKLdRt8">this video about where the phrase dead as a doornail came from</a>.
+        I swear I'm more interesting than this, I'm just really struggling at the moment to think of anything, I'll come back  here when I think of something interesting, in the meantime why not take a minute or two out of your day and watch <a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=1JOwfKLdRt8">this video about where the phrase dead as a doornail came from</a>.
       </p>
     </section>
   )
