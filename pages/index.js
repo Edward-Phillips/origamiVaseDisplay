c414cfda5bf80ea1aa54113e44bdf9a5923c3c7a
edwardPhillips <edward@twine.fm>
2021-11-27 20:42:42 +0000

    adjust index page layout and information

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,18 @@ import styles from './index.module.scss';
 export default function Index() {
   return (
     <div className={styles.indexContainer}>
-      <section>
-        <h2>Home</h2>
+      <section className={styles.descriptionContainer}>
+        <h2>About</h2>
         <p>
           This is a website I am building to improve my skills in frontend development.
         </p>
-        <article>
-          <h3>About me</h3>
+        <article className={styles.articlePanel}>
+          <h3>Who am I?</h3>
           <p>
-            I'm Ed, I am a software engineer, I also fold origami and do terrible drawings. My code is slightly better. Usually.
-          </p>
-          <p>
-            I am a full stack developer, currently working in react and Laravel. Most of my side projects are written in react or node. I have recently been implementing typescript where convenient and am a big fan.
+            I'm Ed, I am a full stack developer, currently working in react and Laravel, I also fold origami and do terrible drawings. Most of my side projects are written in react or node.I have recently been implementing typescript where convenient and am a big fan.
+            </p>
+            <p>
+            I recommend you check out the Origami Vase Design page, it's a project I'm quite proud of to help me design origami vases, or scroll down to have a look at a timeline of my professional life.
           </p>
         </article>
       </section>
