d7d182e2a59cea11f8bdbd70973993ed595d6a81
edwardPhillips <edward@twine.fm>
2021-11-27 20:05:33 +0000

    remove section tag wrapping ProfessionalTimeline

--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,17 @@ export default function Index() {
         <p>
           This is a website I am building to improve my skills in frontend development.
         </p>
-      <article>
-        <h3>About me</h3>
-        <p>
-          I'm Ed, I am a software engineer, I also fold origami and do terrible drawings. My code is slightly better. Usually.
-        </p>
-        <p>
-          I am a full stack developer, currently working in react and Laravel. Most of my side projects are written in react or node. I have recently been implementing typescript where convenient and am a big fan.
-        </p>
-    </article>
+        <article>
+          <h3>About me</h3>
+          <p>
+            I'm Ed, I am a software engineer, I also fold origami and do terrible drawings. My code is slightly better. Usually.
+          </p>
+          <p>
+            I am a full stack developer, currently working in react and Laravel. Most of my side projects are written in react or node. I have recently been implementing typescript where convenient and am a big fan.
+          </p>
+        </article>
       </section>
-      <section>
-        <ProfessionalTimeline />
-      </section>
+      <ProfessionalTimeline />
     </div>
   )
 }
