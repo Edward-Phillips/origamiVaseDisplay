18083d2d33cfa67533f983495f83c4d153f0934d
edwardPhillips <edward@twine.fm>
2021-11-27 19:03:54 +0000

    make timeline component

---
+++ components/Timeline/ProfessionalTimeline.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Chrono } from "react-chrono";
+import timelineData from './TimelineData';
+import styles from './ProfessionalTimeline.module.scss';
+
+export default function ProfessionalTimeline() {
+  return (
+    <div className={styles.timelineContainer}>
+    <Chrono
+      items={timelineData}
+      mode="VERTICAL_ALTERNATING"
+      hideControls
+      disableNavOnKey
+      enableOutline
+      />
+    </div>
+  );
+}
+
---
+++ components/Timeline/TimelineData.js
@@ -0,0 +1,35 @@
+const timelineData = [
+  {
+    title: "2013-2016",
+    cardTitle: "University of Bristol",
+    cardSubtitle:
+      "Aerospace Engineering",
+    cardDetailedText: `I studied Aerospace Engineering at the University of Bristol, a path which I had assumed would be my career, however after studying and being exposed to the industry I realised it wasn't for me.`
+  },
+  {
+    title: "2017",
+    cardTitle: "Brewhouse Bar and Kitchen",
+    cardSubtitle: "My first job: Hospitality",
+    cardDetailedText: "After university I moved back home to London and started working in a pub just behind Angel station called Brewhouse Bar and Kitchen where I worked clearing tables and taking orders, a simpler time."
+  },
+  {
+    title: "2017",
+    cardTitle: "Dr Ed Online Doctors",
+    cardSubtitle: "My second job: Customer Support",
+    cardDetailedText: "Following some conversations with the clientele of Brewhouse I was offered an interview for a customer support role at a medtech, offering remote consultations for treatment"
+  },
+  {
+    title: "2018-9",
+    cardTitle: "Zavamed: Accounts Assistant",
+    cardSubtitle: "My third job",
+    cardDetailedText: "Dr Ed went through a rebrand to Zavamed and I managed to secure a position in the finance team, processing invoices and closing the books every month, it was here that I grew my interest in programming, as a significant portion of my job was happening in google sheets I found myself building dynamic dashboards and template sheets to allow automatic imports into our accounting system"
+  },
+  {
+    title: "2020",
+    cardTitle: "Makers Academy",
+    cardSubtitle: "A 12-week coding bootcamp",
+    cardDetailedText: "I completed a 12 week coding bootcamp at Makers Academy, a course I greatly enjoyed, as it gave me the framework I needed to grow as a developer, with a curriculum focusing on programming concepts like test driven development and agile philosophy, there was also a strong focus on emotional intelligence and effective communication"
+  }
+];
+
+export default timelineData;
