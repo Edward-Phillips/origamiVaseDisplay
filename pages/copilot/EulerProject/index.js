39763dcf92f2758e7daae79696c7c52217d00d1c
edwardPhillips <edward@twine.fm>
2021-11-28 19:12:41 +0000

    TODO: refactor to problemPageLink/PageLink component

--- pages/copilot/EulerProject/index.js
+++ pages/copilot/EulerProject/index.js
@@ -17,6 +17,17 @@ const EulerProject = () => {
         <a href="https://projecteuler.net/">Project Euler</a> is a series of challenging mathematical/computer programming problems.
         This site is a collection of solutions to the problems. Except this time, the problems will be solved by Github Copilot. I will be feeding in a description of the problem to Github Copilot and then the solution will be posted here.
       </p>
+      {
+        Array(7).fill(0).map((_, i) => {
+          return(
+            <div key={`ProjectEulerProblemPages${i}`}>
+              <h2>Problem {i+1}</h2>
+              <p>
+                <a href={`/copilot/EulerProject/${i+1}`}>Solution</a>
+              </p>
+            </div>
+          )
+      })}
     </main>
     </>
   )
