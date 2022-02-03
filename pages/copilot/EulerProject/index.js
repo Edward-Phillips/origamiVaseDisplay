9cd0363a68ed02ad0e9d37cf098f769510460c9e
edwardPhillips <edward@twine.fm>
2022-02-03 18:08:11 +0000

    add grid layout to index page

--- pages/copilot/EulerProject/index.js
+++ pages/copilot/EulerProject/index.js
@@ -1,50 +1,55 @@
-import Head from 'next/head';
-import Layout from '../../../components/LayoutComponents/layout'
-import Sidebar from '../../../components/LayoutComponents/sidebar'
-import { PrismaClient } from '@prisma/client'
+import Head from "next/head";
+import Layout from "../../../components/LayoutComponents/layout";
+import Sidebar from "../../../components/LayoutComponents/sidebar";
+import { PrismaClient } from "@prisma/client";
+import styles from "./index.module.scss";
 
 export async function getStaticProps() {
   const prisma = new PrismaClient();
-  const eulerProblems = await prisma.eulerproblem.findMany()
+  const eulerProblems = await prisma.eulerproblem.findMany();
   prisma.$disconnect();
   return {
     props: {
-      eulerProblems
+      eulerProblems,
     },
-  }
+  };
 }
 
 const EulerProject = (props) => {
-
-  const {eulerProblems} = props;
+  const { eulerProblems } = props;
 
   return (
     <>
-      <Head >
+      <Head>
         <title>Project Euler Problems</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <main>
-      <h1 style={{textAlign: "center"}}>Project Euler Problems</h1>
-      <p>
-        <a href="https://projecteuler.net/">Project Euler</a> is a series of challenging mathematical/computer programming problems.
-        This site is a collection of solutions to the problems. Except this time, the problems will be solved by Github Copilot. I will be feeding in a description of the problem to Github Copilot and then the solution will be posted here.
-      </p>
-      {
-        eulerProblems.map((_, i) => {
-          return(
-            <div key={`ProjectEulerProblemPages${i}`}>
-              <h2>Problem {i+1}</h2>
-              <p>
-                <a href={`/copilot/EulerProject/${i+1}`}>Solution</a>
-              </p>
-            </div>
-          )
-      })}
-    </main>
+      <main className={styles.container}>
+        <h1 style={{ textAlign: "center" }}>Project Euler Problems</h1>
+        <p className={styles.description}>
+          <a href="https://projecteuler.net/">Project Euler</a> is a series of
+          challenging mathematical/computer programming problems. This site is a
+          collection of solutions to the problems. Except this time, the
+          problems will be solved by Github Copilot. I will be feeding in a
+          description of the problem to Github Copilot and then the solution
+          will be posted here.
+        </p>
+        <section className={styles.problemGrid}>
+          {eulerProblems.map((_, i) => {
+            return (
+              <div key={`ProjectEulerProblemPages${i}`}>
+                <h2>Problem {i + 1}</h2>
+                <p>
+                  <a href={`/copilot/EulerProject/${i + 1}`}>Solution</a>
+                </p>
+              </div>
+            );
+          })}
+        </section>
+      </main>
     </>
-  )
-}
+  );
+};
 
 EulerProject.getLayout = function getLayout(page) {
   return (
@@ -52,7 +57,7 @@ EulerProject.getLayout = function getLayout(page) {
       <Sidebar />
       {page}
     </Layout>
-  )
-}
+  );
+};
 
 export default EulerProject;
